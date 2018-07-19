66dfe408adaf2ea96e04ff0bda055400d13f7ba5
mando222 <eric.zietlow@datastax.com>
2018-07-19 14:05:13 -0500

    basic forms input working

--- src/components/account/register/registration-form.js
+++ src/components/account/register/registration-form.js
@@ -5,19 +5,38 @@ import { validateForm } from '../../../lib/validation';
 import Icon from '../../shared/icon';
 import Input from '../../shared/Input';
 
-function handleSubmit (){
-
-}
-
-function handleChange(e, v){
-    console.log(e);
-    console.log(v);
-}
-
 class RegistrationFormContainer extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            formData:{
+                firstName:"",
+                lastName:"",
+                email:"",
+                password:"",
+            },
+        }
+    }
+
+    handleSubmit(formData){
+        console.log(formData)
+    }
+
+    handleChange(event){
+        var value = event.target.value;
+        var key = event.target.name;
+        this.setState({
+            ...this.state,
+            formData: {
+                ...this.state.formData,
+                [key]: value
+            }
+        });
+    }
+
     render() {
         return (
-            <form role="form">
+            <form role="form" onSubmit={this.handleSubmit}>
                 {/*<Alert bsStyle="info" className={error ? 'hidden' : undefined}>*/}
                     {/*Register for an account to upload and comment on videos.*/}
                 {/*</Alert>*/}
@@ -28,33 +47,40 @@ class RegistrationFormContainer extends React.Component {
                 <div id="register-account-fields">
                     <Input
                         controlId="formHorizontalFirstName"
-                        name="First Name"
+                        name="firstName"
+                        label="First Name"
                         type="text"
+                        value={this.state.formData.firstName}
+                        onChange={(v) => { this.handleChange(v)}}
                     />
                     <Input
                         controlId="formHorizontalLastName"
-                        name="Last Name"
+                        name="lastName"
+                        label="Last Name"
                         type="text"
+                        value={this.state.formData.lastName}
+                        onChange={(v) => { this.handleChange(v)}}
                     />
                     <Input
                         controlId="formHorizontalEmail"
-                        name="Email"
+                        name="email"
+                        label="First Name"
                         type="email"
+                        value={this.state.formData.email}
+                        onChange={(v) => { this.handleChange(v)}}
                     />
                     <Input
                         controlId="formHorizontalPassword"
-                        name="Password"
+                        name="password"
+                        label="Password"
                         type="password"
-                    />
-                    <Input
-                        controlId="formHorizontalRetypePassword"
-                        name="Retype Password"
-                        type="password"
+                        value={this.state.formData.password}
+                        onChange={(v) => { this.handleChange(v)}}
                     />
                 </div>
                 <FormGroup>
                     <Col smOffset={2} sm={10}>
-                        <Button type="submit">
+                        <Button onClick={() => {this.handleSubmit(this.state.formData)}}>
                             {/*<Icon name="cog" animate="spin" className={submitting ? undefined : 'hidden'} />*/}
                             Register
                         </Button>
--- src/components/shared/input.js
+++ src/components/shared/input.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Row,Col,FormGroup, FormControl,ControlLabel,HelpBlock} from 'react-bootstrap';
 
+
+// ToDo:
+//add retype password part and validation to go with
+//remove bad value handle from event handler
+
+
 // this input element takes the following props
-// default value - optional default value for the input *Optional*
+// value - value for the input
 // controlId - ids for the elements
 // name - name
 // type - text,file,email,password,
@@ -17,47 +23,109 @@ class Input extends React.Component {
              controlId:props.controlId,
              name:props.name,
              type:props.type,
-             helpblock:props.helpblock ? props.helpblock : ""
+             helpblock:props.helpblock ? props.helpblock : "",
+             handleParentChange:props.onChange,
+             passwordRetype:""
          }
     }
 
-    //custom form validation
+    // custom form validation
     getValidationState() {
         const length = this.state.value.length;
-        if (length > 10) return 'success';
-        else if (length > 5) return 'warning';
+        if (length > 2) return 'success';
+        else if (length > 1) return 'warning';
         else if (length > 0) return 'error';
         return null;
     }
 
-    handleChange(e) {
-        this.setState({ value: e.target.value });
+    handleChange(event) {
+        //extra validation here
+        var value = event.target.value;
+        switch(this.props.type) {
+            case "password":
+                console.log(this.state.passwordRetype)
+                if (this.state.passwordRetype.length > 1 && value == this.state.passwordRetype){
+                    console.log("Passwords Match")
+                    this.props.onChange(event)
+                }else{
+                    //make msg that passwords need to match
+                    console.log("Passwords Do Not Match")
+                    return 'error';
+                }
+                break;
+            case "retype":
+                console.log(this.state.passwordRetype)
+                console.log(this.props.value)
+                if (this.props.value.length > 1 && value == this.props.value){
+                    console.log("Passwords Match")
+                    this.props.onChange(event)
+                }else{
+                    //make msg that passwords need to match
+                    console.log("Passwords Do Not Match")
+                    return 'error';
+                }
+                break;
+            case "text":
+                this.props.onChange(event)
+                break;
+            case "email":
+                this.props.onChange(event)
+                break;
+            case "file":
+                this.props.onChange(event)
+                break;
+            default:
+                console.log("not a valid type for input element")
+        }
     }
 
     render() {
         return (
-            <Row>
-                <FormGroup
-                    controlId={this.state.controlId}
-                    validationState={this.getValidationState()}
-                >
-                    <Col componentClass={ControlLabel} sm={2}>
-                        {this.state.name}
-                    </Col>
-                    <Col sm={10}>
-                        <FormControl
-                            type={this.state.type}
-                            value={this.state.value}
-                            placeholder={this.state.name}
-                            onChange={this.handleChange}
-                        />
-                    </Col>
-                    <HelpBlock>{this.state.helpblock}</HelpBlock>
-                </FormGroup>
-            </Row>
+            <div>
+                <Row>
+                    <FormGroup
+                        controlId={this.props.controlId}
+                        validationState={this.getValidationState()}
+                    >
+                        <Col componentClass={ControlLabel} sm={2}>
+                            {this.props.label}
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl
+                                type={this.props.type}
+                                name={this.props.name}
+                                value={this.props.value}
+                                placeholder={this.props.label}
+                                onChange={(e) => { this.handleChange(e)}}
+                            />
+                        </Col>
+                        <HelpBlock>{this.props.helpblock}</HelpBlock>
+                    </FormGroup>
+                </Row>
+                {/*{this.props.type == "password" ?*/}
+                    {/*<Row>*/}
+                        {/*<FormGroup*/}
+                            {/*controlId={this.props.controlId}*/}
+                            {/*validationState={this.getValidationState()}*/}
+                        {/*>*/}
+                            {/*<Col componentClass={ControlLabel} sm={2}>*/}
+                                {/*Retype Password*/}
+                            {/*</Col>*/}
+                            {/*<Col sm={10}>*/}
+                                {/*<FormControl*/}
+                                    {/*type="password"*/}
+                                    {/*value={this.props.passwordRetype}*/}
+                                    {/*placeholder="Retype Password"*/}
+                                    {/*onChange={(e,v) => { this.handleChange(e,v,this.props.passwordRetype,"retype")}}*/}
+                                {/*/>*/}
+                            {/*</Col>*/}
+                        {/*</FormGroup>*/}
+                    {/*</Row>*/}
+                    {/*:null*/}
+                {/*}*/}
+            </div>
         );
     }
 }
-
 
 export default Input;
